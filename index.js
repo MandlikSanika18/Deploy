6e8090a8d5cf042fb25809113fd76b81d1b843d4
MandlikSanika18 <mandliksanika18@.com>
2025-07-23 23:09:10 +0530

    env

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const PORT = 3000;
+const dotenv = require('dotenv')
+dotenv.config()
+const PORT = 3000 || process.env.PORT;
 const app = express()
 
 app.get("/",(req,res)=>{
